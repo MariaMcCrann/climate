3d0627cec3684e23dce272406d3c3db10ec43200
Ryan J. Parker <ryan@basketballgeek.com>
2013-11-20 17:21:29 -0500

    Test

--- R/plot_2d_9.R
+++ R/plot_2d_9.R
@@ -87,6 +87,7 @@ p.samet <- round(P[row(P)==col(P)],1)
 N.samet <- length(f.samet)
 seq.samet <- c(2, 15, 30, 40, 50, 60, 70, 80, 90, min(n1,n2))
 
+
 if (!("WHICH_CDAT" %in% ls())) {
 	stop("Missing data type")
 }
